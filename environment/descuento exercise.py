e6b13a9c7d71084619c007b9b6cc54a4ec4da33f
Yessica L. Rodriguez Muñoz <89185291+yessicarm@users.noreply.github.com>
2023-03-08 16:30:57 -0600

    add python exercises

---
+++ environment/descuento exercise.py
@@ -0,0 +1,8 @@
+precio = float(input("¿que precio tiene el producto?"))
+
+def restarDescuento(precio):
+    descuento= precio*0.15 
+    precioFinal= precio-descuento 
+    return precioFinal 
+    
+    
